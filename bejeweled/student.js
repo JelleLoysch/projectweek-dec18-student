906a19a95c3785daedfbb8c6cc1c46a3245d34ed
Jelle Loysch <jelle.loysch@student.ucll.be>
2018-12-19 10:59:33 +0100

    Update bejeweld mvc

--- bejeweled/student.js
+++ bejeweled/student.js
@@ -1,1 +1,31 @@
-// Schrijf hier je code
+function width(grid){
+	return grid[0].length
+}
+
+function height(grid){
+	return grid.length
+}
+
+function isInside(grid, position) {
+	let w = width(grid);
+	let h = height(grid);
+
+	if (position.x < 0 || position.y < 0) {
+		return false;
+	}
+
+	return h > position.y && w > position.x;
+}
+	
+function swap(grid,p,q){
+    console.log(grid);
+    
+    let temp= grid[q.y][q.x];
+    
+    grid[q.y][q.x]= grid[p.y][p.x];
+    grid[p.y][p.x]=temp;
+    
+    
+
+   
+}
