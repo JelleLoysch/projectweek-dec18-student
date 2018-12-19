71242a0946a0a5d40335f5a8efe20e355b7e463e
r0745013 <43341205+r0745013@users.noreply.github.com>
2018-12-19 11:34:03 +0100

    update

--- bejeweled/student.js
+++ bejeweled/student.js
@@ -1,1 +1,47 @@
-// Schrijf hier je code
+function width(grid) {
+	return grid[0].length;
+}
+
+function height(grid) {
+	return grid.length;
+}
+
+function isInside(grid, position) {
+	let w = width(grid);
+	let h = height(grid);
+
+	if (position.x < 0 || position.y < 0) {
+		return false;
+	}
+
+	return h > position.y && w > position.x;
+}
+
+function swap(grid, p, q) {
+	let w = width(grid);
+	let h = height(grid);
+
+}
+
+function swap(grid,p,q){
+    let temp= grid[q.y][q.x];
+    
+    grid[q.y][q.x]= grid[p.y][p.x];
+    grid[p.y][p.x]=temp;
+}
+
+function horizontalChainAt(grid, position) {
+	return grid[0].length;
+}
+
+function verticalChainAt(grid, position)  {
+	return grid[0].length;
+}
+
+function removeChains(grid) {
+
+}
+
+function collapse(grid) {
+	
+}
